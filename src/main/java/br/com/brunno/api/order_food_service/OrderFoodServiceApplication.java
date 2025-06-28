be2d2db283adc85b6354e2efa0e684cfce2449f2
Brunno Santana <brunnosantana98@gmail.com>
2025-06-28 13:55:33 -0300

    inital commit

---
+++ src/main/java/br/com/brunno/api/order_food_service/OrderFoodServiceApplication.java
@@ -0,0 +1,13 @@
+package br.com.brunno.api.order_food_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderFoodServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderFoodServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/brunno/api/order_food_service/OrderFoodServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.brunno.api.order_food_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderFoodServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
